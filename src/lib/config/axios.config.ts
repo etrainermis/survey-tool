0d14da613a2845f3e2ea40f59764319d9ec3e101
U-Alice <umugwanezaalice22@gmail.com>
2025-03-25 18:56:35 +0200

    [update]:.env

--- src/lib/config/axios.config.ts
+++ src/lib/config/axios.config.ts
@@ -2,11 +2,11 @@ import _ from "axios";
 
 const baseUrl = import.meta.env.DEV
 
-  ? import.meta.env.VITE_BASE_API_URL_PROD
+  ? import.meta.env.VITE_BASE_API_URL_LOCAL
 
   : import.meta.env.VITE_BASE_API_URL;
 
-console.log({baseUrl});
+console.log({ baseUrl });
 
 const axios = _.create({
   baseURL: baseUrl,
